fa538e3735c3ebccb01d1001d6486667263d2564
powerdreams-1 <95529902+powerdreams-1@users.noreply.github.com>
2021-12-10 01:46:19 +0300

    Update index.php

---
+++ index.php
@@ -0,0 +1,13 @@
+<!IDOCTYPE html>
+<html>
+<head>
+<title>PHP store </title>
+</head>
+<body>
+<?php
+// using two foward slashes we create comments in php.
+// within the php tag,we can insert valuesinto our HTML.
+echo"<hi>welcome! </hi>";
+?>
+</body>
+</html>
