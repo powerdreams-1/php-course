ad5167e3585ca166a76a37a1c5f3c546753f401d
powerdreams-1 <95529902+powerdreams-1@users.noreply.github.com>
2021-12-10 08:59:06 +0300

    Update index.php

--- index.php
+++ index.php
@@ -5,9 +5,13 @@
 </head>
 <body>
 <?php
+$name="PHP store"
+$credit=1000;
+
 // using two foward slashes we create comments in php.
 // within the php tag,we can insert valuesinto our HTML.
-echo"<hi>welcome! </hi>";
+echo"<hi>welcome to ".$name."!</hi>";
+echo "<h2>you have $".$credit ."in your wallet.</h2>";
 ?>
 </body>
 </html>
